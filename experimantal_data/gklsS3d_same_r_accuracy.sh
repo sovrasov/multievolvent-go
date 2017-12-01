072fb604bf8c64f7e0aad5bb1b3b42af10e87b76
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2017-12-01 16:46:46 +0300

    Add report, rename folder with the exp results

--- experimantal_data/gklsS3d_same_r_accuracy.sh
+++ experimantal_data/gklsS3d_same_r_accuracy.sh
@@ -3,9 +3,9 @@
 cd ./gklsS3d/same_r/accuracy && \
 #../../../../build/multievolvent-sample -c gklsS -d 3 -t rotated -e 0.002 -q 5 -a -E 0.05 -l 1 -r 4.7 -s -i 500000
 #../../../../build/multievolvent-sample -c gklsS -d 3 -t rotated -e 0.002 -q 5 -a -E 0.05 -l 2 -r 4.7 -s -i 500000
-#../../../../build/multievolvent-sample -c gklsS -d 3 -t rotated -e 0.002 -q 5 -a -E 0.05 -l 3 -r 4.7 -s -i 500000
-#../../../../build/multievolvent-sample -c gklsS -d 3 -t rotated -e 0.002 -q 5 -a -E 0.05 -l 4 -r 4.7 -s -i 500000
+../../../../build/multievolvent-sample -c gklsS -d 3 -t rotated -e 0.002 -q 5 -a -E 0.05 -l 3 -r 4.7 -s -i 500000
+../../../../build/multievolvent-sample -c gklsS -d 3 -t rotated -e 0.002 -q 5 -a -E 0.05 -l 4 -r 4.7 -s -i 500000
 #../../../../build/multievolvent-sample -c gklsS -d 3 -t shifted -e 0.002 -q 5 -a -E 0.05 -l 1 -r 4.7 -s -i 500000
 ../../../../build/multievolvent-sample -c gklsS -d 3 -t shifted -e 0.002 -q 5 -a -E 0.05 -l 2 -r 4.7 -s -i 500000
-#../../../../build/multievolvent-sample -c gklsS -d 3 -t shifted -e 0.002 -q 5 -a -E 0.05 -l 3 -r 4.7 -s -i 500000
-#../../../../build/multievolvent-sample -c gklsS -d 3 -t shifted -e 0.002 -q 5 -a -E 0.05 -l 4 -r 4.7 -s -i 500000
+../../../../build/multievolvent-sample -c gklsS -d 3 -t shifted -e 0.002 -q 5 -a -E 0.05 -l 3 -r 4.7 -s -i 500000
+../../../../build/multievolvent-sample -c gklsS -d 3 -t shifted -e 0.002 -q 5 -a -E 0.05 -l 4 -r 4.7 -s -i 500000
