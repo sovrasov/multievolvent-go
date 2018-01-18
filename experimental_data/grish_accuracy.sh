a56d138f446fbcabbe315923813fb2f01082414e
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-01-18 14:55:17 +0300

    Fix some bash scripts

--- experimental_data/grish_accuracy.sh
+++ experimental_data/grish_accuracy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ./f_gr/accuracy && ../../../build/multievolvent-sample -c grish -t rotated -e 0.001 -a -l 1 -r 3.0 -s -i 15000
+cd ./f_gr/accuracy &&
+../../../build/multievolvent-sample -c grish -t rotated -e 0.001 -a -l 1 -r 3.0 -s -i 15000
 ../../../build/multievolvent-sample -c grish -t rotated -e 0.001 -a -l 2 -r 2.3 -s -i 15000
 ../../../build/multievolvent-sample -c grish -t rotated -e 0.001 -a -l 3 -r 2.1 -s -i 15000
 ../../../build/multievolvent-sample -c grish -t shifted -e 0.001 -a -l 1 -r 3.1 -s -i 15000
