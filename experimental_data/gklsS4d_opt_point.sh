eaac006e8efd6815bfb43bc3b3bf86045d972ca0
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-02-20 11:08:05 +0300

    New experimants with multilevel evolvent

--- experimental_data/gklsS4d_opt_point.sh
+++ experimental_data/gklsS4d_opt_point.sh
@@ -2,4 +2,6 @@
 
 cd ./gklsS4d/opt_point && \
 ../../../build/multievolvent-sample -c gklsS -d 4 -t rotated    -e 0.01 -r 4.5 -q 5 -s -i 250000
+../../../build/multievolvent-sample -c gklsS -d 4 -t multilevel -e 0.01 -r 4.5 -q 5 -s -i 250000 -m 8
 ../../../build/multievolvent-sample -c gklsS -d 4 -t multilevel -e 0.01 -r 4.5 -q 5 -s -i 250000 -m 9
+../../../build/multievolvent-sample -c gklsS -d 4 -t multilevel -e 0.01 -r 4.5 -q 5 -s -i 250000 -m 10
