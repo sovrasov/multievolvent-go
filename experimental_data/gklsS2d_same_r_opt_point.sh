71d85f442a40a1109a4d000ed5c0adc3bc1140ac
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2018-04-03 12:14:41 +0300

    Add new data with smooth evolvents

--- experimental_data/gklsS2d_same_r_opt_point.sh
+++ experimental_data/gklsS2d_same_r_opt_point.sh
@@ -10,3 +10,4 @@ cd ./gklsS2d/same_r/opt_point && \
 ../../../../build/multievolvent-sample -c gklsS -t shifted -e 0.01 -l 4 -r 5.0 -E 0.05 -s -i 170000
 
 ../../../../build/multievolvent-sample -c gklsS -t noninjective -e 0.01 -i 10000 -r 5.0 -s
+../../../../build/multievolvent-sample -c gklsS -t smooth -e 0.01 -i 60000 -r 5.0 -m 10 -s
