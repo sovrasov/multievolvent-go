8b0213df0c4fea0aa569a787779533bc34795d6f
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2019-04-06 22:46:19 +0300

    Add support of 3d evolvents

--- scripts/plot_evolvent.py
+++ scripts/plot_evolvent.py
@@ -5,10 +5,36 @@ import argparse
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 matplotlib.rcParams['mathtext.fontset'] = 'cm'
 matplotlib.rcParams['mathtext.rm'] = 'serif'
+
+def plot_2d_evolvent(points, style):
+    assert points.shape[1] == 2
+
+    x, y = points[:,0], points[:,1]
+
+    fig = plt.figure()
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    axes.plot(x, y, style, alpha = .8)
+    axes.set_xlabel('$x$', fontsize=14)
+    axes.set_ylabel('$y$', fontsize=14)
+    axes.grid(linestyle='dashed', linewidth=0.5)
+
+def plot_3d_evolvent(points, style):
+    assert points.shape[1] == 3
+    x, y, z = points[:,0], points[:,1], points[:,2]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(x, y, z)
+    ax.set_xlabel('$x$', fontsize=14)
+    ax.set_ylabel('$y$', fontsize=14)
+    ax.set_zlabel('$z$', fontsize=14)
+    ax.grid(linestyle='dashed', linewidth=0.5)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -22,24 +48,21 @@ def main():
     colors = ('b', 'g', 'r', 'c', 'm', 'y', 'k', 'w')
     linestyles = ['-', '--', '-.', ':', ' ', '', ':', '_']
 
-    fig = plt.figure()
-    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-
     for i, points_file in enumerate(args.points_files):
-        x, y = [], []
+        points = []
         with open(points_file, 'r') as f:
             for line in f.readlines()[1:]:
-                points = line.strip().split(',')
-                x.append(float(points[0]))
-                y.append(float(points[1]))
+                coordinates = line.strip().split(',')
+                point = []
+                for coord in coordinates:
+                    point.append(float(coord))
+                points.append(point)
 
-        x, y = np.array(x), np.array(y)
-
-        axes.plot(x, y, colors[i] + linestyles[i], alpha = .8)
-
-        axes.set_xlabel('$x$', fontsize=14)
-        axes.set_ylabel('$y$', fontsize=14)
-        axes.grid(linestyle='dashed', linewidth=0.5)
+        points = np.array(points)
+        if points.shape[0] == 2:
+            plot_2d_evolvent(points, colors[i] + linestyles[i])
+        else:
+            plot_3d_evolvent(points, colors[i] + linestyles[i])
 
     if args.show:
         plt.show()
